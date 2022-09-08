fe363133c317de928227555a8957442fed85f2f9
Lucas <lhqmalta@gmail.com>
2022-09-08 17:45:09 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { datadogRum } from '@datadog/browser-rum';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    datadogRum.startView({ service: 'my-service', name: 'my-service' })
+  }, [])
+
+  const fetchSomething = () => {
+    fetch("http://httpstat.us/500")
+  }
+
+  const changeRoute = () => {
+    setCounter(c => {
+      window.history.pushState("", "", `/new-route${counter}`);
+      return c + 1;
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchSomething}>Fetch something</button>
+      <button onClick={changeRoute}>Change route</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,8 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { datadogRum } from '@datadog/browser-rum';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+datadogRum.init({
+  applicationId: process.env.REACT_APP_DATADOG_APP_ID,
+  clientToken: process.env.REACT_APP_DATADOG_CLIENT_TOKEN,
+  site: process.env.REACT_APP_DATADOG_SITE,
+  service: 'default',
+  env: process.env.NODE_ENV,
+  sampleRate: 100,
+  premiumSampleRate: 100,
+  trackInteractions: true,
+  trackViewsManually: true, // <--- This is the important part
+  defaultPrivacyLevel: 'mask-user-input',
+  beforeSend: (event, context) => {
+    console.log(event.view.name, event.service);
+
+    if (event.type === 'resource' && event.resource.status_code >= 400 && event.resource.type === 'fetch') {
+      event.context = {
+        ...event.context,
+        url: window.location.href
+      };
+      console.log('sending error to -> ', event.view.name);
+    }
+  },
+});
+
+datadogRum.startSessionReplayRecording();
+
 root.render(
   <React.StrictMode>
     <App />
